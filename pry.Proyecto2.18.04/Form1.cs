c6d12bb6e89cde3a7060cfb3d1460adf28a6fd15
Valentin Lopez <112416951+Valenlopez14@users.noreply.github.com>
2023-04-18 11:36:51 -0300

    Add files via upload

---
+++ pry.Proyecto2.18.04/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pry.Proyecto2._18._04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SFguardar.FileName = "";
+            SFguardar.ShowDialog();
+            string archivo = SFguardar.FileName;
+
+
+            StreamWriter sw = new StreamWriter(archivo);
+            sw.WriteLine(textBox1.Text);
+            sw.Close();
+            sw.Dispose();
+            textBox1.Text = "";
+        }
+    }
+}
